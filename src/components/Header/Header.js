7ddde840327e21655e6af970299e806d840320e0
Santhosh Sivaji <santhosh_s@ambattur.com>
2022-09-12 14:34:34 +0530

    Header js changes
    12-Sep

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,6 +26,13 @@ export default function Header() {
 
   const [visible, setVisible] = useState(false);
   const [userApps, setUserApps] = useState([]);
+  const [shProfileCard, setshProfileCard] = useState(false);
+  const [searchclass, setsearchclass] = useState("");
+
+  const showhideProfileCard = async () => {
+    setshProfileCard(value => !value);
+  }
+
 
   const showDrawer = () => {
     setVisible(true);
@@ -111,6 +118,12 @@ export default function Header() {
   }, []);
 
 
+  const SearchBox_Event = name => (e) => {
+    let value = e.target.value;
+    if (value.length > 0) setsearchclass("search-box-forms");
+    else setsearchclass("");
+  }
+
   const logOut = async () => {
     let result = await ItrAuthService.logout();
     if (result.Success == true) {
@@ -127,66 +140,76 @@ export default function Header() {
   // const [swi,setSwi] = useState(true);
 
   return (
-    <div className='header-main'>
-      {/* <Switch checked={swi} onChange={(e) => setSwi(e == true ? 'Y' : 'N')} /> */}
-      {/* <h6> {swi == true ? 'Active' : 'Disable'} </h6> */}
-      <div className='row header align-items-center'>
-        <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 left'>
-          <div className='left-menu'>
-            <div className='profile-card' onClick={showDrawer}>
-              <img src={ConfigurationLogo} width="100%" />
-            </div>
-            <div className='profile-names'>
-              <h6>Configuration Management</h6>
+
+    <>
+      <div class="main-header sticky side-header nav nav-item">
+        <div class="container-fluid">
+          <div class="main-header-left ">
+            <ul class="nav nav-item  navbar-nav-right ms-auto" style={{ "width": "250px" }}>
+              <li class="dropdown main-profile-menu nav nav-item nav-link">
+                <a class="profile-user d-flex">
+                  <img src={HeaderLogo} alt="" onClick={showDrawer} />
+                  <div class="ms-2 my-auto">
+                    <h6>iThred</h6>
+                    <span>Configuration</span>
+                  </div>
+                  <i class="angle fe fe-chevron-down"></i>
+                </a>
+              </li>
+            </ul>
+            <div class="main-header-center ms-4 d-sm-none d-md-none d-lg-block">
+              <input class={`form-control ${searchclass}`} placeholder="Search With Keywords" type="search" name='search' onChange={SearchBox_Event("search")} />
+              <button class="btn">
+                <i class="fas fa-search d-none d-md-block"></i>
+              </button>
             </div>
           </div>
-        </div>
-
-        {/* <div className='col-6 col-sm-4 col-md-4 col-lg-5 col-xl-5 middle'>
-          <div className='search-box'>
-            <input type="search" placeholder='Search With Keywords' className='form-control form-control-sm' />
-            <img className='search-icon' src={SearchIcon} width="15" />
-          </div>
-        </div> */}
-
-        <div className='col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3'>
-          <div className='search-box'>
-            <select className='form-select form-select-sm'
-              value={locationCode}
-              onChange={(e) => {
-                setLocationCode(e.target.value);
-                changeLocation(e.target.value)
-              }}
-            >
-              <option value=""> Select Location</option>
-              {LocationData.map((data, index) => {
-                return <option key={index} value={data.locationCode}>{data.locationCode}</option>
-              })}
-            </select>
-          </div>
-        </div>
-
-        <div className='col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 right'>
-          <Popover placement="bottomRight" title="" content={
-            <button className='btn btn-sm text-danger px-4 py-0' onClick={logOut}>Logout</button>
-          }>
-            <div className='right-menu'>
-              <div className='profile-right-card'>
-                <img src={Avatar} width="100%" />
-              </div>
-              <div className='profile-right-names'>
-                <h6> {userProfile.firstName} {userProfile.lastName} </h6>
-                <p> {dt} </p>
-                {/* <p>{moment(userProfile.createdDate).format("D/M/YYYY,h:mm")}</p> */}
-              </div>
-              <div className='right-arrow-down'>
-                <FontAwesomeIcon icon={faChevronDown} color="white" size="xs" />
+          <div class="main-header-right">
+            
+            <div className='col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5'>
+              <div className='search-box'>
+                <select className='form-select form-select-sm' value={locationCode} onChange={(e) => { setLocationCode(e.target.value); changeLocation(e.target.value) }} >
+                  <option value=""> Select Location</option>
+                  {
+                    LocationData.map((data, index) => {
+                      return <option key={index} value={data.locationCode}>{data.locationCode}</option>
+                    })
+                  }
+                </select>
               </div>
             </div>
-          </Popover>
+
+            <ul class="nav nav-item  navbar-nav-right ms-auto">
+              <li class="dropdown main-profile-menu nav nav-item nav-link">
+                <a class="profile-user d-flex" onClick={showhideProfileCard} >
+                  <img alt="" src={Avatar} />
+                  <div class="ms-2 my-auto">
+                    <h6>{userProfile.displayName}</h6>
+                    <span>{userProfile.userType}</span>
+                  </div>
+                  <i class="angle fe fe-chevron-down"></i>
+                </a>
+                <div class="dropdown-menu" style={{ "display": (shProfileCard ? "block" : "none") }}>
+                  <a class="dropdown-item" ><i class="bx bx-user-circle"></i>Profile</a>
+                  <a class="dropdown-item" ><i class="bx bx-cog"></i> Edit Profile</a>
+                  {/* <a class="dropdown-item" ><i class="bx bxs-inbox"></i>Inbox</a>
+                  <a class="dropdown-item" ><i class="bx bx-envelope"></i>Messages</a>
+                  <a class="dropdown-item" ><i class="bx bx-slider-alt"></i>
+                    Account Settings
+                  </a>
+                  <a class="dropdown-item pos-relative t-siz">
+                    <img src="assets/img/textaa.svg" /> Text size <span class="w-cv"> <span class="s-but">A</span> <span class="n-but">A</span> <span class="l-but">A</span></span>
+                    </a> */}
+                  <a class="dropdown-item" onClick={logOut}>
+                    <i class="bx bx-log-out"></i>
+                    Sign Out
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-
       <Drawer className="drawer-main" closable={false} title={
         <div className='drawer-header'>
           {/* <img src={DrawerLogo} width="25" /> */}
@@ -212,10 +235,7 @@ export default function Header() {
           <div className='drawer-body-container mt-4'>
             <div className='row'>
               {userApps.map((apps, index) => {
-                return <div key={index} className='col-sm-6 col-md-6 col-lg-6 col-xl-6 mt-4 menu-items'
-                  onClick={() => window.location.href = `http://${apps.hostUIURL}/`}>
-                  {/* onClick={() => apps.serviceCode == 'ENAPP002' ? window.location.href = 'http://localhost:82/' : apps.serviceCode == 'ENAPP003' ? window.location.href = 'http://localhost:82/' : apps.serviceCode == 'ENAPP001' ? window.location.href = 'http://localhost:83/' : window.location.href = 'http://localhost:83/'}> */}
-                  {/* return <div key={index} className='col-sm-6 col-md-6 col-lg-6 col-xl-6 mt-4 menu-items' onClick={() => { apps.serviceCode == 'ENAPP002' ? window.location.href = '/ProductDevelopment' : apps.serviceCode == 'ENAPP003' ? window.location.href = '/ConfigurationManagement' : apps.serviceCode == 'ENAPP004' ? window.location.href = '/QualityAssurance' : window.location.href = '/PreProduction' }}> */}
+                return <div key={index} className='col-sm-6 col-md-6 col-lg-6 col-xl-6 mt-4 menu-items' onClick={() => window.location.href = `http://${apps.hostUIURL}/`}>
                   <div className='menu-list '>
                     <div className='menu-img'>
                       <img src={userAppIcons.icons} width="40" />
@@ -226,33 +246,97 @@ export default function Header() {
                   </div>
                 </div>
               })}
-
-              {/* <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 mt-4 menu-items' onClick={() => window.location.href = '/QualityManagement'}>
-                <div className='menu-list '>
-                  <div className='menu-img'>
-                    <img src={Quality} width="40" />
-                  </div>
-                  <div className='menu-title px-2'>
-                    <h6>Quality Management</h6>
-                  </div>
-                </div>
-              </div>
-
-              <div className='col-sm-6 col-md-6 col-lg-6 col-xl-6 mt-4 menu-items' onClick={() => window.location.href = '/ConfigurationManagement'}>
-                <div className='menu-list '>
-                  <div className='menu-img'>
-                    <img src={Configuration} width="40" />
-                  </div>
-                  <div className='menu-title px-2'>
-                    <h6>Configuration Management</h6>
-                  </div>
-                </div>
-              </div> */}
-
             </div>
           </div>
         </div>
       </Drawer>
-    </div>
+    </>
+    // <div className='header-main'>
+    //   <div className='row header align-items-center'>
+    //     <div className='col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 left'>
+    //       <div className='left-menu'>
+    //         <div className='profile-card' onClick={showDrawer}>
+    //           <img src={ConfigurationLogo} width="100%" />
+    //         </div>
+    //         <div className='profile-names'>
+    //           <h6>Configuration Management</h6>
+    //         </div>
+    //       </div>
+    //     </div>
+    // <div className='col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3'>
+    //   <div className='search-box'>
+    //     <select className='form-select form-select-sm' value={locationCode} onChange={(e) => { setLocationCode(e.target.value); changeLocation(e.target.value) }} >
+    //       <option value=""> Select Location</option>
+    //       {
+    //         LocationData.map((data, index) => {
+    //           return <option key={index} value={data.locationCode}>{data.locationCode}</option>
+    //         })
+    //       }
+    //     </select>
+    //   </div>
+    // </div>
+
+    //     <div className='col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 right'>
+    //       <Popover placement="bottomRight" title="" content={
+    //         <button className='btn btn-sm text-danger px-4 py-0' onClick={logOut}>Logout</button>
+    //       }>
+    //         <div className='right-menu'>
+    //           <div className='profile-right-card'>
+    //             <img src={Avatar} width="100%" />
+    //           </div>
+    //           <div className='profile-right-names'>
+    //             <h6> {userProfile.firstName} {userProfile.lastName} </h6>
+    //             <p> {dt} </p>
+    //           </div>
+    //           <div className='right-arrow-down'>
+    //             <FontAwesomeIcon icon={faChevronDown} color="white" size="xs" />
+    //           </div>
+    //         </div>
+    //       </Popover>
+    //     </div>
+    //   </div>
+
+    //   <Drawer className="drawer-main" closable={false} title={
+    //     <div className='drawer-header'>
+    //       <p className='px-2 menu-back-button' onClick={onClose}>
+    //         <span>
+    //           <FontAwesomeIcon icon={faArrowLeft} />
+    //         </span>  Back
+    //       </p>
+    //     </div>
+    //   } placement="left" onClose={onClose} visible={visible}>
+    //     <div className='drawer-body-main mt-3'>
+    //       {/* header */}
+    //       <div className='drawer-body-header '>
+    //         <div className='header-img p-1'>
+    //           <img src={DrawerLogo} width="40" />
+    //         </div>
+    //         <div className='header-title px-3'>
+    //           <h6>Garmenting <br /> Enterprise Apps</h6>
+    //         </div>
+    //       </div>
+
+    //       {/* menu-items */}
+    //       <div className='drawer-body-container mt-4'>
+    //         <div className='row'>
+    //           {userApps.map((apps, index) => {
+    //             return <div key={index} className='col-sm-6 col-md-6 col-lg-6 col-xl-6 mt-4 menu-items'
+    //               onClick={() => window.location.href = `http://${apps.hostUIURL}/`}>
+    //               <div className='menu-list '>
+    //                 <div className='menu-img'>
+    //                   <img src={userAppIcons.icons} width="40" />
+    //                 </div>
+    //                 <div className='menu-title px-2'>
+    //                   <h6 id={apps.serviceCode}> {apps.serviceName} </h6>
+    //                 </div>
+    //               </div>
+    //             </div>
+    //           })}             
+
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </Drawer>
+    // </div>
   )
 }
